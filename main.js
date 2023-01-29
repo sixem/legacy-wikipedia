815af64810a7d22d94ad759537bdd857ba8dcea7
emy <emy@five.sh>
2023-01-29 22:18:48 +0100

    Improve logic

--- main.js
+++ main.js
@@ -3,23 +3,21 @@
 	/** Get current location */
 	const currentLocation = document.location.href;
 
-	if(!currentLocation.includes('useskin=vector'))
+	/** New URL object */
+	const currentUrl = new URL(currentLocation);
+
+	if(!(currentUrl.searchParams.get('useskin') === 'vector'))
 	{
 		/** If no `useskin=vector` parameter is set, set it */
-		const url = new URL(currentLocation);
-
-		url.searchParams.set('useskin', 'vector');
+		currentUrl.searchParams.set('useskin', 'vector');
 
 		/** Redirect page */
-		window.location.replace(url);
+		window.location.replace(currentUrl);
 	} else {
-		/** New URL object */
-		const url = new URL(window.location.href);
-
 		/** Remove the `useskin` parameter */
-		url.searchParams.delete('useskin');
+		currentUrl.searchParams.delete('useskin');
 
 		/** Hide the parameter from the URL without reloading the page */
-		window.history.replaceState(null, null, url);
+		window.history.replaceState(null, null, currentUrl);
 	}
 })();
