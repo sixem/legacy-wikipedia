b34913c314ff82bad14e1f6e1245d52ca0921681
emy <emy@five.sh>
2023-01-29 20:22:46 +0100

    Move packaged files into a separate directory

--- zip.sh
+++ zip.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+mkdir -p "packages" && rm "packages/"*
+
 ## Chrome
-7z a "legacy-wikipedia_chrome.zip" 48.png 128.png main.js manifest.json
+7z a "packages/legacy-wikipedia_chrome.zip" 48.png 128.png main.js manifest.json
 
 mv manifest.json .manifest.json
 mv .ff_requires/manifest.json manifest.json
 
 ## Firefox
-7z a "legacy-wikipedia_firefox.zip" 48.png 128.png main.js manifest.json
+7z a "packages/legacy-wikipedia_firefox.zip" 48.png 128.png main.js manifest.json
 
 mv manifest.json .ff_requires/manifest.json
 mv .manifest.json manifest.json
