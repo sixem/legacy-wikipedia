200a6ea18e52e17d05f1b6ada486d8a79313128b
emy <emy@five.sh>
2023-04-09 16:56:44 +0200

    Utilize `webNavigation` when adding `GET` parameters

--- main.js
+++ main.js
@@ -17,76 +17,22 @@
 			? stored
 			: fallback;
 	};
-
+	
 	/** Get current location */
 	const currentLocation = document.location.href;
 
 	/** New URL object */
 	const currentUrl = new URL(currentLocation);
 
-	if(currentUrl.searchParams.get('useskin') === null)
-	{
-		/**
-		 * This is a sort of "hacky" way of stopping the page from flashing
-		 * with the default style before it's changed.
-		 * 
-		 * This happens because we are using asynchronous functions to access
-		 * stored settings, and the page is loaded before the settings are retrieved, thus
-		 * we get a quick default style flash before the page is handled by the addon.
-		 * 
-		 * This stops the page from loading if the `useskin` parameter is not set at all.
-		 * 
-		 * This assumes that it'll always be set by the addon once it has been handled.
-		 */
-		window.stop();
-	}
-
 	/** Get stored settings */
 	const hideParameters = await getStoredSetting('hide-get-parameters', true);
-	const excludedSubdomains = await getStoredSetting('exclude-versions', null);
 
-	/** Get current skin */
-	let currentSkin = await getStoredSetting('skin', 'vector');
-
-	/** Check if the current subdomain is excluded */
-	if(excludedSubdomains
-		&& excludedSubdomains.length > 0)
+	if(currentUrl.searchParams.get('useskin') && hideParameters)
 	{
-		const matches = /([^\/\.]+)\.wikipedia\.org\/wiki\//g.exec(currentLocation);
+		/** Remove the `useskin` parameter */
+		currentUrl.searchParams.delete('useskin');
 
-		if(matches)
-		{
-			const subDomain = matches[1].toLowerCase();
-
-			if(excludedSubdomains.split(',').map((s) => s.trim().toLowerCase()).includes(subDomain))
-			{
-				/**
-				 * If the subdomain is excluded, use the default skin
-				 * 
-				 * There is no "default" skin, but since we're assuming the addon
-				 * to always set a skin, we can use that as the default, as it'll
-				 * have no effect on the page.
-				 */
-				currentSkin = 'default';
-			}
-		}
-	}
-
-	if(!(currentUrl.searchParams.get('useskin') === currentSkin))
-	{
-		/** If no `useskin=vector` parameter is set, set it */
-		currentUrl.searchParams.set('useskin', currentSkin);
-
-		/** Redirect page */
-		window.location.replace(currentUrl);
-	} else {
-		if(hideParameters)
-		{
-			/** Remove the `useskin` parameter */
-			currentUrl.searchParams.delete('useskin');
-
-			/** Hide the parameter from the URL without reloading the page */
-			window.history.replaceState(null, null, currentUrl);
-		}
+		/** Hide the parameter from the URL without reloading the page */
+		window.history.replaceState(null, null, currentUrl);
 	}
 })();
--- service.js
+++ service.js
@@ -10,7 +10,7 @@ const options = {
 		default: false,
 		current: null
 	},
-    'skin': {
+	'skin': {
 		type: 'selection',
 		default: 'vector',
 		current: null
@@ -36,15 +36,15 @@ Object.keys(options).forEach((key) =>
  */
 const updateCurrentOptions = async () =>
 {
-    for(const key in options)
-    {
-        await chrome.storage.local.get(key, (result) =>
-        {
-            options[key].current = result[key];
-        });
-    }
+	for(const key in options)
+	{
+		await chrome.storage.local.get(key, (result) =>
+		{
+			options[key].current = result[key];
+		});
+	}
 
-    return options;
+	return options;
 };
 
 /**
@@ -58,25 +58,25 @@ const optionsGet = (args) =>
 /**
  * `onMessage` listener
  */
-chrome.runtime.onMessage.addListener(async (data, sender, sendResponse) =>
+chrome.runtime.onMessage.addListener((data, sender, sendResponse) =>
 {
 	/** Task IDs and their corresponding functions */
 	let tasks = {
 		optionsGet: optionsGet,
-        getStoredSetting: async (args) =>
-        {
-            const storedValue = options[args.data.key]?.current;
+		getStoredSetting: async (args) =>
+		{
+			const storedValue = options[args.data.key]?.current;
 
-            return args.sendResponse(storedValue !== null
-                ? storedValue
-                : await chrome.storage.local.get(args.data.key)
-            );
-        },
-        updateCurrentOptions: async (args) =>
-        {
-            await updateCurrentOptions();
-            return args.sendResponse(options);
-        },
+			return args.sendResponse(storedValue !== null
+				? storedValue
+				: await chrome.storage.local.get(args.data.key)
+			);
+		},
+		updateCurrentOptions: async (args) =>
+		{
+			await updateCurrentOptions();
+			return args.sendResponse(options);
+		},
 	};
 
 	if(tasks[data.task])
@@ -84,9 +84,94 @@ chrome.runtime.onMessage.addListener(async (data, sender, sendResponse) =>
 		/** Perform task */
 		tasks[data.task]({ data, sender, sendResponse });
 	}
-
-	return true;
 });
 
 /** Update current values */
 updateCurrentOptions();
+
+/**
+ * Fetches a stored setting
+ * 
+ * @param {string} key 
+ */
+const getStoredSetting = async (key, fallback = null) =>
+{
+	const storedValue = options[key]?.current;
+
+	const keyValue = storedValue !== null
+		? storedValue
+		: await chrome.storage.local.get(key);
+
+	return keyValue !== null ? keyValue : fallback;
+};
+
+let expectRedirect = false;
+
+const urlFilter = { url: [
+	{ hostSuffix: 'wikipedia.org', pathPrefix: '/wiki/' },
+	{ hostSuffix: 'wikipedia.org', pathPrefix: '/w/' }
+]};
+
+const handleTab = async (tab) =>
+{
+	/** Get current location */
+	const currentLocation = tab.url;
+
+	/** New URL object */
+	const currentUrl = new URL(currentLocation);
+
+	/** Get stored settings */
+	const excludedSubdomains = await getStoredSetting('exclude-versions', null);
+
+	/** Get current skin */
+	let currentSkin = await getStoredSetting('skin', 'vector');
+
+	/** Check if the current subdomain is excluded */
+	if(excludedSubdomains
+		&& excludedSubdomains.length > 0)
+	{
+		const matches = /([^\/\.]+)\.wikipedia\.org\/wiki\//g.exec(currentLocation);
+
+		if(matches)
+		{
+			const subDomain = matches[1].toLowerCase();
+
+			if(excludedSubdomains.split(',').map((s) => s.trim().toLowerCase()).includes(subDomain))
+			{
+				/** Don't use a skin if site is excluded */
+				currentSkin = null;
+			}
+		}
+	}
+
+	/** Set parameters that'd indicate a redirection */
+	expectRedirect = [
+		'search'
+	].map((p) => currentUrl.searchParams.get(p)).filter((p) => p).length > 0 ? true : false;
+
+	if((!(currentUrl.searchParams.get('useskin') === currentSkin) && currentSkin !== null))
+	{
+		/** If no `useskin=vector` parameter is set, set it */
+		currentUrl.searchParams.set('useskin', currentSkin);
+
+		/** Redirect page */
+		chrome.tabs.update(tab.tabId, {
+			url: currentUrl.href
+		});
+	}
+};
+
+chrome.webNavigation.onBeforeNavigate.addListener(async (tab) =>
+{
+	handleTab(tab);
+}, urlFilter);
+
+chrome.webNavigation.onCommitted.addListener(async (tab) =>
+{
+	/** Handle expected redirections */
+	if(expectRedirect
+		&& tab.transitionQualifiers.includes('server_redirect'))
+	{
+		handleTab(tab);
+	}
+}, urlFilter);
